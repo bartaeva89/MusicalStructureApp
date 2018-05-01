184f2a2b1fdc0cf55ef914b2f147e68b9863ac97
Barta Éva Anna <barta.eva@gmail.com>
2018-05-01 20:58:06 +0200

    app with buttons

--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,21 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ArrayList<Song> songs = new ArrayList<Song>();
+
         songs.add(new Song("Zauber Flöte", "Mozart"));
         songs.add(new Song("Four season", "Beethowen"));
         songs.add(new Song("Mer Hahn en neue Oberkeet", "Bach"));
         SongAdapter adapter = new SongAdapter(this, songs);
+        final Intent changeIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
         final ListView listView = (ListView) findViewById(R.id.song_list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent changeIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                Button startButton=(Button)findViewById(R.id.play_button);
+                startButton.setVisibility(View.VISIBLE);
                 Song music = (Song) listView.getItemAtPosition(i);
                 changeIntent.putExtra("tittle", music.getSongName());
                 changeIntent.putExtra("artist", music.getArtistName());
-                startActivity(changeIntent);
+                startButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(changeIntent);
+                    }
+                });
+
             }
         });
+
     }
 }
--- MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
+++ MusicalStructureApp/app/src/main/java/com/example/android/musicalstructureapp/NowPlayingActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructureapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class NowPlayingActivity extends AppCompatActivity {
@@ -12,8 +15,17 @@ public class NowPlayingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_now_playing);
         TextView actualAuthor=(TextView) findViewById(R.id.actual_author);
         TextView actualTittle=(TextView) findViewById(R.id.actual_tittle);
+        Button stopButton=(Button)findViewById(R.id.stop_button);
         Bundle bundle = getIntent().getExtras();
         actualAuthor.setText(bundle.getString("artist"));
         actualTittle.setText(bundle.getString("tittle"));
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent stopIntent = new Intent(NowPlayingActivity.this, MainActivity.class);
+                startActivity(stopIntent);
+            }
+        });
+
     }
 }
